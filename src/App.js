d07aef488e28f25bdb09ef7025b6fd640d39b8cb
VictoriaBubbleGun <jennyfer.mueller@hotmail.com>
2023-09-21 22:14:14 +0200

    added a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Dictionary from "./dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="text-center">hello</header>
+      <Dictionary />
+      <footer className="text-center">
+        {" "}
+        This project was coded by Jennyfer Müller and is open-sourced on{" "}
+        <a href="https://github.com/VictoriaBubbleGun/Dictionary">GitHub</a>
+      </footer>
     </div>
   );
 }
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function keywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
-    <div className="Dictionary">
-      <h1>Dictionary</h1>
+    <div className="Dictionary m-3">
+      <h1 className="text-center">Dictionary</h1>
+      <form onSubmit={search} className="text-center m-4">
+        <input type="search" onChange={keywordChange} autoFocus="on" />
+      </form>
     </div>
   );
 }
