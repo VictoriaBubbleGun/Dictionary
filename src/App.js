3622bea8830d388e66438795813dbde829e908ff
VictoriaBubbleGun <jennyfer.mueller@hotmail.com>
2023-09-21 21:44:22 +0200

    set up component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="text-center">hello</header>
     </div>
   );
 }
